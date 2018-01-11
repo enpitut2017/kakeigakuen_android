26d7e3bd2322220b9d3d13243b3b9efc080b6fab
Tomoya Mikami <trymorecc@gmail.com>
2018-01-12 02:44:09 +0900

    volleyの追加

---
+++ app/src/main/java/xyz/kakeigakuen/kakeityan_agent_android/LoginUtil.kt
@@ -0,0 +1,18 @@
+package xyz.kakeigakuen.kakeityan_agent_android
+
+/**
+ * Created by paseri on 2018/01/12.
+ */
+class LoginUtil {
+
+    val domain: String
+
+    constructor(d: String) {
+        this.domain = d
+    }
+
+    constructor() {
+        this.domain = "kakigakuen.xyz"
+    }
+
+}
