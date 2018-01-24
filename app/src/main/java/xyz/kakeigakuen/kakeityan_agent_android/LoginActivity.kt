ea914ec08fbfca0a3d4854a3848222efff031287
tomoya mikami <trymorecc@gmail.com>
2018-01-24 14:37:17 +0900

    login処理完成

--- app/src/main/java/xyz/kakeigakuen/kakeityan_agent_android/LoginActivity.kt
+++ app/src/main/java/xyz/kakeigakuen/kakeityan_agent_android/LoginActivity.kt
@@ -1,5 +1,7 @@
 package xyz.kakeigakuen.kakeityan_agent_android
 
+import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -8,6 +10,11 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity
+import com.trello.rxlifecycle.kotlin.bindToLifecycle
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+import xyz.kakeigakuen.kakeityan_agent_android.client.LoginClient
+import xyz.kakeigakuen.kakeityan_agent_android.generator.HttpGenerator
 
 class LoginActivity : RxAppCompatActivity() {
 
@@ -19,8 +26,29 @@ class LoginActivity : RxAppCompatActivity() {
     fun onClickLogin(view: View) {
         val email: EditText = findViewById(R.id.email)
         val password: EditText = findViewById(R.id.password)
-        val loginutil = LoginUtil()
-        loginutil.login(email.text.toString(), password.text.toString(), view)
-        Log.i("result", loginutil.set_result.toString())
+        val httpgenerator = HttpGenerator()
+        val loginclient = httpgenerator.retrofit.create(LoginClient::class.java)
+
+        Log.i("action", "loginstart")
+        loginclient.login(email.text.toString(), password.text.toString())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .bindToLifecycle(view)
+                .subscribe({
+                    if (it.token.toString() != "error") {
+                        Log.i("action", "login_success")
+                        val prefer = getSharedPreferences("user", Context.MODE_PRIVATE)
+                        val editor = prefer.edit()
+                        editor.putString("token", it.token)
+                        editor.putInt("budget", it.budget)
+                        editor.commit()
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Log.i("action", "you miss email or password")
+                    }
+                }, {
+                    Log.i("action", "login_failed")
+                })
     }
 }
